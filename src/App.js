b174a76a9048be179d027ee2d78120483ea3bb75
David Erazo <da.erazom@gmail.com>
2019-04-22 23:27:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+// Import component
+import Header from './components/Header';
+import Clientes from './components/Clientes';
+import EditarCliente from './components/EditarCliente';
+import NuevoCliente from './components/NuevoCliente';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  onError: ({ networkError, graphqlErros }) => {
+    console.log('graphQLErrors', graphqlErros);
+    console.log('networkError', networkError);
+  }
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <Router>
+          <Fragment>
+            <Header/>
+                <div className="container">
+                  <Switch>
+                    <Route exact path="/" component={Clientes}/>
+                    <Route exact path="/cliente/nuevo" component={NuevoCliente}/>
+                    <Route exact path="/cliente/editar/:id" component={EditarCliente}/>
+                  </Switch>
+                </div>
+          </Fragment>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
